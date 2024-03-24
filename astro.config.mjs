c4b5b99d2a651dfe95d8a049c659b5d06067bc22
Fredrik Svärd <haffy@icloud.com>
2024-03-24 21:46:11 +0100

    Removed www from adress.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 
 export default defineConfig({
-  site: 'https://www.njordstudio.com'
+  site: 'https://njordstudio.com'
 })
