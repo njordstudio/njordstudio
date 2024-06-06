aff0ed506a69794e76602f7c7f19b0be0036c60b
sirhaffy <haffy@icloud.com>
2024-06-07 00:25:47 +0200

    styling changes

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,6 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
+    const navigation = document.querySelector('.navigation');
+    if (navigation) {
+        navigation.classList.toggle('expanded');
+    }
 });
