afc136ea9237b2c15ea4298d19a7d8529db703ff
Fredrik Svärd <haffy@icloud.com>
2024-06-06 10:35:46 +0200

    added new features like mdx support and stories, about and more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+
 
 // https://astro.build/config
-
 export default defineConfig({
-  site: 'https://njordstudio.com'
-})
+  site: 'https://njordstudio.com',
+  integrations: [mdx()],
+  output: 'static',
+  image: {
+    service: squooshImageService(),
+  },
+});
