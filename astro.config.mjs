e1bc14bcc63706a417aecff61ec3c3e3f17501a8
Fredrik Svärd <haffy@icloud.com>
2024-06-11 13:32:28 +0200

    Testing CDN for blurhash

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import { imageService } from "@unpic/astro/service";
 export default defineConfig({
   site: 'https://njordstudio.com',
   integrations: [mdx()],
-  output: 'static',
   image: {
     service: imageService({
       fallbackService: "squooshImageService",
