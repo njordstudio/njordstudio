353569cfebf88235385e6e4c2a85d57abb29f1d9
sirhaffy <haffy@icloud.com>
2024-07-18 11:45:13 +0200

    Trying to get GIT Actions to work

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,13 +16,7 @@ export default defineConfig({
   ],
   output: 'static',
   // Workaround to fix bug in WSL
-  vite: {
-    server: {
-      watch: {
-        usePolling: true,
-      },
-    },
-  },
+
   image: {
     service: passthroughImageService(),
   }
